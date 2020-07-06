588c419ce3fe9eb0c4e286894c5c5ae90e7441a8
hunterhector <hunterhector@gmail.com>
2020-07-06 01:40:10 -0400

    Update indent to 2 spaces.

--- src/plugins/dialogue_box/TextInput.tsx
+++ src/plugins/dialogue_box/TextInput.tsx
@@ -4,77 +4,77 @@ import {ISinglePack} from "../../nlpviewer"
 import {OnEventType} from "./DialogueBox"
 
 export interface TextInputProp{
-    textValue: string;
-    textPack: ISinglePack;
-    onEvent?: OnEventType;
+  textValue: string;
+  textPack: ISinglePack;
+  onEvent?: OnEventType;
 }
 
 interface TextInputState{
-    textValue: string;
+  textValue: string;
 }
 
 function submitUtterance(textPack: ISinglePack, newUtterance: string, onEvent?: OnEventType){
-    let {text} = textPack;
+  let {text} = textPack;
 
-    text = text + '\n' + newUtterance;
-    const end = text.length;
-    const begin = text.length - newUtterance.length;
+  text = text + '\n' + newUtterance;
+  const end = text.length;
+  const begin = text.length - newUtterance.length;
 
-    if (onEvent){
-        onEvent(
-            {
-                type: 'new-utterance', // TODO: add strick type for event
-                text: text,
-                span: {
-                    begin: begin,
-                    end: end,
-                },
-                legendId: "ft.onto.base_ontology.Utterance",
-                attributes: {
-                    "speaker": "user",
-                },
-            }
-        )
-    }
+  if (onEvent){
+    onEvent(
+      {
+        type: 'new-utterance', // TODO: add strick type for event
+        text: text,
+        span: {
+          begin: begin,
+          end: end,
+        },
+        legendId: "ft.onto.base_ontology.Utterance",
+        attributes: {
+          "speaker": "user",
+        },
+      }
+    )
+  }
 }
 
 class TextInput extends React.Component<TextInputProp, TextInputState>{
-    constructor(props: TextInputProp){
-        super(props);
-        this.state = {
-            textValue: ''
-        };
-    }
-
-    render(){
-        return <form onSubmit={e => {e.preventDefault();}} >
-            <label>
-                <input className={style.field} type="text" value={this.state.textValue} onChange={e => {
-                    this.setState({textValue: e.target.value})
-                }}  
-                onKeyUp={e => {
-                    if (e.key === "Enter"){
-                        submitUtterance(
-                            this.props.textPack, 
-                            this.state.textValue,
-                            this.props.onEvent,
-                        );
-                        this.setState({textValue:''});
-                    }
-                }}
-                />                
-            </label>
-            <input type="button" value="Submit" onClick={
-                e => {
-                    submitUtterance(
-                        this.props.textPack, 
-                        this.state.textValue,
-                        this.props.onEvent);
-                    this.setState({textValue:''});
-                }}
-            />
-        </form>
+  constructor(props: TextInputProp){
+    super(props);
+    this.state = {
+      textValue: ''
     };
+  }
+
+  render(){
+    return <form onSubmit={e => {e.preventDefault();}} >
+      <label>
+        <input className={style.field} type="text" value={this.state.textValue} onChange={e => {
+          this.setState({textValue: e.target.value})
+        }}  
+        onKeyUp={e => {
+          if (e.key === "Enter"){
+            submitUtterance(
+              this.props.textPack, 
+              this.state.textValue,
+              this.props.onEvent,
+            );
+            this.setState({textValue:''});
+          }
+        }}
+        />                
+      </label>
+      <input type="button" value="Submit" onClick={
+        e => {
+          submitUtterance(
+            this.props.textPack, 
+            this.state.textValue,
+            this.props.onEvent);
+          this.setState({textValue:''});
+        }}
+      />
+    </form>
+  };
 }
 
 export default TextInput;
