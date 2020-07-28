b3522a8e8de1cabcc6d3dc816269d701bf2acffb
stevezhang1999 <stevezhang1999@gmail.com>
2020-07-28 11:39:48 +0800

    fix comments
    Former-commit-id: a61efa51b7c7b5721943c54e937fb2803a271646

--- simple-backend/nlpviewer_backend/handlers/project.py
+++ simple-backend/nlpviewer_backend/handlers/project.py
@@ -61,3 +61,4 @@ def delete(request, project_id):
     project.delete()
 
     return HttpResponse('ok')
+    
--- simple-backend/test.py
+++ simple-backend/test.py
@@ -3,16 +3,16 @@ from nlpviewer_backend.models import Document, Project
 
 class DocumentTestCase(TestCase):
     def setUp(self):
-        a = Project.objects.create(name="project1")
+        a = Project.objects.create(name="project1",
+                                   ontology='I am ontology')
 
         Document.objects.create(name="doc1", 
                                 textPack='I am text pack',
-                                ontology='I am ontology',
                                 project = a)
         
 
     def test_project_relationship(self):
-        #"""Animals that can speak are correctly identified"""
+        """test project relationship"""
         doc1 = Document.objects.get(name="doc1")
         project1 = Project.objects.get(name="project1")
         project1.documents.first()
