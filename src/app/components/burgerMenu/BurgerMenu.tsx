c48f672f29ef169955e4b575576f3c5a8bb053a5
Yue Cen <cyrexin@gmail.com>
2020-08-16 01:27:36 -0400

    Add hamburger menu.
    Former-commit-id: 6bd35ddc94b4bcefd07ecc4f7eaefcd8c2ac9a0d

---
+++ src/app/components/burgerMenu/BurgerMenu.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { StyledBurger } from './StyledBurger';
+import { StyledMenu } from './StyledMenu';
+
+class BurgerMenu extends React.Component<{open: boolean, setOpen: Function}> {
+  private menuRef = React.createRef<HTMLDivElement>();
+
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClickOutside);
+  }
+
+  handleClickOutside = (event: Event) => {
+    if (!this.props.open) {
+      return;
+    }
+
+    if (this.menuRef.current && !this.menuRef.current.contains(event.target as Node)) {
+      this.props.setOpen(false);
+    }
+  }
+
+  render() {
+    const childrenWithProps = React.Children.map(this.props.children, child => {
+      const props = { onClick: () => this.props.setOpen(!this.props.open) };
+      if (React.isValidElement(child)) {
+          return React.cloneElement(child, props);
+      }
+      return child;
+    });
+    
+    return (
+      <div ref={this.menuRef}>
+        <StyledBurger open={this.props.open} 
+                      onClick={() => this.props.setOpen(!this.props.open)}>
+        <span />
+        <span />
+        <span />
+      </StyledBurger>
+      <StyledMenu open={this.props.open}>
+        {childrenWithProps}
+      </StyledMenu>
+    </div>  
+    )
+  }
+}
+
+export default BurgerMenu;
---
+++ src/app/components/burgerMenu/StyledMenu.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const StyledMenu = styled.div<{ open: boolean }>`
+  background-color: black;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  left: 0;
+  padding: 60px;
+  position: absolute;
+  top: 0;
+  transform: ${({ open }) => open ? 'translateX(0)' : 'translateX(-100%)'};
+  transition: transform 0.3s ease-in-out;
+  
+  a {
+    color: white;
+    cursor: pointer;
+    font-size: 20px;
+    margin: 15px 0;
+    text-decoration: none;
+  }
+`;
