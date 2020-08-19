4aed2eb384fac39cb6ef3f1ecba86abb069c4c74
MC <58713140+nagiifan@users.noreply.github.com>
2020-08-19 19:04:56 -0400

    Update the UI of login page (#97)
    * Add login page
    
    * Update Login.tsx
    
    * Update Login.tsx
    
    * Update App.tsx
    
    * Update App.tsx
    
    * Fix "ReferenceError: Link is not defined"
    
    * Roll back changes on App.tsx
    
    * Remove the "Remember me" part and update the logo url
    
    * logo update
    
    1. used public url
    2. removed the uploaded image

--- src/app/pages/Login.tsx
+++ src/app/pages/Login.tsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 import { login } from '../lib/api';
 import { useHistory } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
 
 function Login() { 
   const [error, setError] = useState<string>('');
@@ -23,35 +29,81 @@ function Login() {
     history.push("/signup") 
   }
 
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(15),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(3),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+
+    const classes = useStyles();
+
   return (
-    <div>
-      <h2>Login</h2>
+      <Container component="main" maxWidth="xs">
+        <div className={classes.paper}>
+          <img alt='stave logo' src={process.env.PUBLIC_URL + '/Stave-dark-text@1x.png'}/>
+          <form className={classes.form} noValidate onSubmit={handleLogin}>
 
-      <form onSubmit={handleLogin}>
-        <div>
-          <input
-            placeholder="username"
-            onChange={e => setUsername(e.target.value)}
-            value={username}
-            name="username"
-          />
+                <TextField
+                  onChange={e => setUsername(e.target.value)}
+                  value={username}
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="name"
+                  label="Username"
+                  name="name"
+                  autoComplete="name"
+                  autoFocus
+                />
+
+                <TextField
+                  onChange={e => setPassword(e.target.value)}
+                  value={password}
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="password"
+                  label="Password"
+                  name="password"
+                />
+
+            {error ? <div>{error}</div> : null}
+
+            <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            >
+              Sign In
+            </Button>
+
+            <Grid container>
+              <Grid item>
+                <Link 
+                  component="button"
+                  variant="body2"
+                  onClick={redirectSignup} >
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
         </div>
-
-        <div>
-          <input
-            placeholder="password"
-            onChange={e => setPassword(e.target.value)}
-            value={password}
-            name="password"
-          />
-        </div>
-
-        {error ? <div>{error}</div> : null}
-
-        <button onClick={handleLogin}>login</button>
-        <button onClick={redirectSignup}>sign up</button>
-      </form>
-    </div>
+      </Container>
   );
 }
 
