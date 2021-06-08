55d543af911110ce8045e27f95412dd301788910
hunterhector <hunterhector@gmail.com>
2021-06-07 21:35:21 -0400

    fix version

--- simple-backend/nlpviewer_backend/__version__.py
+++ simple-backend/nlpviewer_backend/__version__.py
@@ -14,7 +14,7 @@
 
 _MAJOR = "0"
 _MINOR = "0"
-_REVISION = "1dev1"
+_REVISION = "1dev10"
 
 VERSION_SHORT = "{0}.{1}".format(_MAJOR, _MINOR)
 VERSION = "{0}.{1}.{2}".format(_MAJOR, _MINOR, _REVISION)
