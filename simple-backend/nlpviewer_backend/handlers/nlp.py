34609098ebaf47ba538f776e51403ab591d5bb42
Suqi Sun <suqi.sun@mbp00482s-MacBook-Pro.local>
2021-06-15 14:12:17 -0400

    Use RemoteProcessor

--- simple-backend/nlpviewer_backend/handlers/nlp.py
+++ simple-backend/nlpviewer_backend/handlers/nlp.py
@@ -26,67 +26,67 @@ except ImportError:
 
 nlp_models = {}
 
-def __load_eliza():
-  if forte_installed:
-    from forte.processors.misc import RemoteProcessor
-    from forte.data.readers import RawDataDeserializeReader
-
-    #Create the pipeline and add the processor.
-    pipeline = Pipeline[DataPack]()
-    pipeline.set_reader(RawDataDeserializeReader())
-    pipeline.add(RemoteProcessor())    
-    pipeline.initialize()
-    return pipeline
-  else:
+def __load_pipeline(url: str = "http://localhost:8008"):
+  if not forte_installed:
     logging.info(forte_msg)
     return None
 
+  from forte.processors.misc import RemoteProcessor
+  from forte.data.readers import RawDataDeserializeReader
 
-def __load_utterance_searcher():
-  if forte_installed:
-    try:
-      from examples.clinical_pipeline.utterance_searcher import LastUtteranceSearcher
-      from forte.data.readers import RawDataDeserializeReader
-    except ImportError:
-      logging.error(
-        "Additional Forte examples: "
-        "https://github.com/asyml/forte/tree/master/examples "
-        "needed to be installed to run this example.")
-      return None
+  #Create the pipeline and add the processor.
+  pipeline = Pipeline[DataPack]()
+  pipeline.set_reader(RawDataDeserializeReader())
+  pipeline.add(RemoteProcessor(), config={"url": url})
+  pipeline.initialize()
+  return pipeline
+
+def __load_eliza(url: str = "http://localhost:8009"):
+  """
+  The remote Forte pipeline should look like:
+
+    from forte.processors.eliza_processor import ElizaProcessor
+    from forte.data.readers import RawDataDeserializeReader
+
+    Pipeline[DataPack]() \
+      .set_reader(RawDataDeserializeReader()) \
+      .add(ElizaProcessor()) \
+      .serve(host="localhost", port=8009)
+  """
+  return __load_pipeline(url=url)
+
+
+def __load_utterance_searcher(url: str = "http://localhost:8010"):
+  """
+  The remote Forte pipeline should look like:
+
+    from examples.clinical_pipeline.utterance_searcher import LastUtteranceSearcher
+    from forte.data.readers import RawDataDeserializeReader
 
     # Load several configuration from environment.
     stave_db_path = os.environ.get('stave_db_path')
     url_stub = os.environ.get('url_stub')
     query_result_project_id = os.environ.get('query_result_project_id')
 
-    #Create the pipeline and add the processor.
-    pipeline = Pipeline[DataPack]()
-    pipeline.set_reader(RawDataDeserializeReader())
-    pipeline.add(LastUtteranceSearcher(),
-      config={
-        'stave_db_path': stave_db_path,
-        'url_stub': url_stub,
-        'query_result_project_id': int(query_result_project_id)
-      }
-    )
-    
-    pipeline.initialize()
-    return pipeline
-  else:
-    logging.info(forte_msg)
-    return None
+    Pipeline[DataPack]() \
+      .set_reader(RawDataDeserializeReader()) \
+      .add(LastUtteranceSearcher(),
+        config={
+          'stave_db_path': stave_db_path,
+          'url_stub': url_stub,
+          'query_result_project_id': int(query_result_project_id)
+        }
+      ) \
+      .serve(host="localhost", port=8010)
+  """
+  return __load_pipeline(url=url)
 
-def __load_content_rewriter():
-  if forte_installed:
-    try:
-      from forte_examples.content_rewriter.rewriter import ContentRewriter
-      from forte.data.readers import RawDataDeserializeReader
-    except ImportError as e:
-      logging.error(
-        "Additional Forte examples: "
-        "https://github.com/asyml/forte/tree/master/forte_examples "
-        "needed to be installed to run this example.")
-      return None
+def __load_content_rewriter(url: str = "http://localhost:8011"):
+  """
+  The remote Forte pipeline should look like:
+
+    from forte_examples.content_rewriter.rewriter import ContentRewriter
+    from forte.data.readers import RawDataDeserializeReader
 
     model_path = os.environ.get('content_rewriter_model_path')
     if not model_path:
@@ -97,19 +97,14 @@ def __load_content_rewriter():
         )
       return None
     else:
-      # Create the pipeline and add the processor
-      pipeline = Pipeline[DataPack]()
-      pipeline.set_reader(RawDataDeserializeReader())
-      pipeline.add(ContentRewriter(), config={
-        'model_dir': os.environ.get('content_rewriter_model_path')
-      })
-
-      # Models gets initialize.
-      pipeline.initialize()
-      return pipeline
-  else:
-    logging.info(forte_msg)
-    return None
+      Pipeline[DataPack]() \
+        .set_reader(RawDataDeserializeReader()) \
+        .add(ContentRewriter(), config={
+          'model_dir': os.environ.get('content_rewriter_model_path')
+        }) \
+        .serve(host="localhost", port=8011)
+  """
+  return __load_pipeline(url=url)
 
 
 @require_login
